9f1885d54655372bccb6799f44a5022a5ec37e85
Pelumi-98 <144035846+Pelumi-98@users.noreply.github.com>
2024-02-06 10:36:37 +0000

    home page bg added

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -6,7 +6,6 @@ const HomePage = () => {
         <video id="background-video" autoPlay loop muted>
           <source src="https://api-www.louvre.fr/sites/default/files/videos/video_1_1_1280/video.mp4" type="video/mp4" /> 
         </video>
-        <AppStructure />
       </div>
     );
   };
