65be3709621e92b939b9ce198f33398041dfeec9
Pelumi-98 <144035846+Pelumi-98@users.noreply.github.com>
2024-02-08 14:21:41 +0000

    tile images removed from resources/images and moved to public dir

--- frontend/src/pages/MuseumsPage.js
+++ frontend/src/pages/MuseumsPage.js
@@ -10,18 +10,22 @@ const MuseumPage = () => {
         
         <section className="pages-museum tile-margin">
           <p className="sub-heading">Pages</p>
+          <img src='/pages-tile-image.jpg' alt='pages-image' height="350px" width="300px"/>
         </section>
 
         <section className="muse-museum">
           <p className="sub-heading">Muse</p>
+          <img src='/muse-tile-image-second.jpg' alt='muse-image' height="350px" width="300px"/>
         </section>
 
         <section className="xplore-museum">
           <p className="sub-heading">Xplore</p>
+          <img src='/xplore-tile-image.jpg' alt='xplore-image' height="350px" width="300px"/>
         </section>
 
         <section className="horizons-museum">
           <p className="sub-heading">Horizons</p>
+          <img src='/horizons-tile-image-second.jpg' alt='horizons-image' height="350px" width="300px"/>
         </section>
 
       </section>
