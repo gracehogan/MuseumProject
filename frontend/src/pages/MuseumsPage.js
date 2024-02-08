579405a9a9b930378b5b7aa1869dd33177946f93
Pelumi-98 <144035846+Pelumi-98@users.noreply.github.com>
2024-02-08 11:04:04 +0000

    template added for Museum main page

--- frontend/src/pages/MuseumsPage.js
+++ frontend/src/pages/MuseumsPage.js
@@ -8,16 +8,16 @@ const MuseumPage = () => {
       <h2 className="page-heading margin-align">Museums</h2>
       <section className="margin-align">
         <section className="pages-museum">
-
+          {/* Pages column here */}
         </section>
         <section className="muse-museum">
-
+          {/* Muse column here */}
         </section>
         <section className="xplore-museum">
-
+          {/* Xplore column here */}
         </section>
         <section className="horizons-museum">
-
+          {/* Horizon column here */}
         </section>
       </section>
     </div>
